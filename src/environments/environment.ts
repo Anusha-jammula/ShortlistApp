c5bbe60eac52a159fc36bfc186ec0f79128f7788
Anusha <anushajammula02@gmail.com>
2025-09-13 13:07:58 +0530

    url link changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: '/assets/json-data/form-submissions.json',
+  apiUrl: 'assets/json-data/form-submissions.json',
   enableLogging: true,
   enablePerformanceMonitoring: false
 };
